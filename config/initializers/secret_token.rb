fa22a13afce3b3a641cd54498f10629efe473421
root <root@3daystartup.org>
2011-08-18 05:42:48 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaryWebsite::Application.config.secret_token = 'b46cae57572f9d0a9ecc1c03f88e5da560593cf2e38cb0023560cc6f2acb2c99ef51e16154e17a1b8f063f1dcbf9c0335d88b4d637b868f9119783dcd01c0f7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GaryWebsite::Application.config.session_store :cookie_store, :key => '_gary_website_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GaryWebsite::Application.config.session_store :active_record_store
